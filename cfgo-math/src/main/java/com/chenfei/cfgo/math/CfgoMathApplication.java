812785afb945f9a6eae6c4925cda9254061e5c79
chenfei <464498258@qq.com>
2023-12-05 15:13:18 +0800

    新建math工程，新增30以内的加法

---
+++ cfgo-math/src/main/java/com/chenfei/cfgo/math/CfgoMathApplication.java
@@ -0,0 +1,13 @@
+package com.chenfei.cfgo.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgoMathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfgoMathApplication.class, args);
+    }
+
+}
---
+++ cfgo-math/src/test/java/com/chenfei/cfgo/math/CfgoMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenfei.cfgo.math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfgoMathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
